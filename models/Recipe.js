b8d51930be2065d33eaddd7d649bcdf8dec4ed5d
Balazs Danyi <danyi.balage@gmail.com>
2020-08-05 15:53:49 +0100

    basic backend

---
+++ models/Recipe.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Create Schema
+const RecipeSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = Recipe = mongoose.model('recipe', RecipeSchema);
